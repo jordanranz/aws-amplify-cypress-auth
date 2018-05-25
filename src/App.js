364fa081528895da3d5ea6985ffa174effd325e0
Jordan Ranz <jranz@amazon.com>
2018-05-25 07:59:00 -0700

    Added withAuthenticator for cypress test

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Amplify, { Auth } from 'aws-amplify';
+import { withAuthenticator } from 'aws-amplify-react';
+import aws_exports from './aws-exports';
+Amplify.configure(aws_exports);
+
 class App extends Component {
   render() {
     return (
@@ -18,4 +23,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default withAuthenticator(App, true);
